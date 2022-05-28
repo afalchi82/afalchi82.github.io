f63a7323a772bd9373b5c248d5058d1fa6ab0b6f
Alessandro Falchi <afalchi82@gmail.com>
2022-05-28 15:01:24 +0200

    react

---
+++ piano-gym/src/components/Options/Options.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Options.css';
+
+export default () => {
+    const options = [1, 2, 3, 4];
+
+    return <div className='options'>
+        { now }
+    </div>;
+};
